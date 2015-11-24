410427961fb21f150cf2004dd85088f6bb733ce4
Syouhei Tanaka <shou.tanak@gmail.com>
2015-11-24 11:11:20 +0900

    Initial commit, bootstrap入れた状態

---
+++ index.php
@@ -0,0 +1,12 @@
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>WSD Blog</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/blog.css">
+</head>
+<body>
+
+<script src="./js/bootstrap.min.js"></script>
+</body>
+</html>
