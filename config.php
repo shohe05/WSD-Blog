2826b7d11f619b37dbf6cf624f60c9386945bf1b
Syouhei Tanaka <shou.tanak@gmail.com>
2015-11-30 22:59:12 +0900

    Modify config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 return [
     'db' => [
-        'database' => 'wsd',
-        'user' => 'root',
-        'password' => 'rootroot',
-        'host' => 'localhost'
+        'database' => 'DATABASE NAME', // データベースの名前
+        'user' => 'USERNAME', // MySQLのユーザー名
+        'password' => 'PASSWORD', // MySQLのパスワード
+        'host' => 'HOST' //MySQLのホスト
     ]
 ];
